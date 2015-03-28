4944e76bfcbfbd049a98435155c525831c72d3a5
kallaballa <amir@viel-zu.org>
2015-03-28 12:11:13 +0100

    formatting

--- src/lounge/lua/mplayer.lua
+++ src/lounge/lua/mplayer.lua
@@ -13,8 +13,8 @@ local PID, STDIN, STDOUT, STDERR = Janosh:popen("/usr/bin/mplayer","-idle", "-in
 Janosh:setenv("http_proxy","")
 
 function basename(str)
-	local name = string.gsub(str, "(.*/)(.*)", "%2")
-	return name
+  local name = string.gsub(str, "(.*/)(.*)", "%2")
+  return name
 end
 
 local MplayerClass = {} -- the table representing the class, which will double as the metatable for the instances
@@ -25,23 +25,23 @@ function MplayerClass.new()
 end
 
 function MplayerClass.jump(self, idx) 
-		obj = Janosh:get("/playlist/items/.")
-		if tonumber(idx) > #obj then
-			idx = #obj
-		end
-		file = obj[tonumber(idx)].url
-		self:cmd("pause")
-		Janosh:trigger("/player/active", "true")
-		self:cmd("loadfile " .. file)
-		Janosh:trigger("/playlist/index", idx)
+    obj = Janosh:get("/playlist/items/.")
+    if tonumber(idx) > #obj then
+      idx = #obj
+    end
+    file = obj[tonumber(idx)].url
+    self:cmd("pause")
+    Janosh:trigger("/player/active", "true")
+    self:cmd("loadfile " .. file)
+    Janosh:trigger("/playlist/index", idx)
 end
 
 function MplayerClass.enqueue(self, videoUrl, title, srcUrl) 
-	if title == "" then
-		title = "(no title)"
-	end
-	size = Janosh:size("/playlist/items/.")
-	Janosh:mkobj("/playlist/items/#" .. size .. "/.")
+  if title == "" then
+    title = "(no title)"
+  end
+  size = Janosh:size("/playlist/items/.")
+  Janosh:mkobj("/playlist/items/#" .. size .. "/.")
   Janosh:set("/playlist/items/#" .. size .. "/url", videoUrl)
   Janosh:set("/playlist/items/#" .. size .. "/title", title)
   Janosh:set("/playlist/items/#" .. size .. "/source", srcUrl)
@@ -50,38 +50,38 @@ end
 function MplayerClass.add(self, videoUrl, title, srcUrl)
   self:enqueue(videoUrl, title, srcUrl)
 
-	Janosh:trigger("/notify/message", "Playing " .. title)
-	if Janosh:get("/player/active").player.active == "false" then
-		self:jump(10000000) -- jump to the ned of the playlist
+  Janosh:trigger("/notify/message", "Playing " .. title)
+  if Janosh:get("/player/active").player.active == "false" then
+    self:jump(10000000) -- jump to the ned of the playlist
   end
 end
 
 function MplayerClass.run(self) 
-	SOTRACK="DEMUXER: ==> Found"
-	EOTRACK="GLOBAL: EOF code: 1"
-	PATH_CHANGED="GLOBAL: ANS_path="
-	CACHEEMPTY="Cache empty"
+  SOTRACK="DEMUXER: ==> Found"
+  EOTRACK="GLOBAL: EOF code: 1"
+  PATH_CHANGED="GLOBAL: ANS_path="
+  CACHEEMPTY="Cache empty"
 
-	while true do
+  while true do
     line=""
-		logf = io.open("/var/log/mplayer.log", "w")
+    logf = io.open("/var/log/mplayer.log", "w")
 print("output")
-		while true do
-			line = Janosh:preadLine(STDOUT)
-			if line == nil then break end	
-		  logf:write(line .. "\n")
-			logf:flush()
-			if string.find(line, EOTRACK) then
-				self:eotrack()
-			elseif string.find(line, SOTRACK) then
+    while true do
+      line = Janosh:preadLine(STDOUT)
+      if line == nil then break end 
+      logf:write(line .. "\n")
+      logf:flush()
+      if string.find(line, EOTRACK) then
+        self:eotrack()
+      elseif string.find(line, SOTRACK) then
         self:sotrack()
       elseif string.find(line, CACHEEMPTY) then
         self:cache_empty()
-			end
-		end
-		--:close()
+      end
+    end
+    --:close()
     Janosh:sleep(1000)
-	end
+  end
 end
 
 function MplayerClass.cmd(self, cmdstring) 
@@ -96,14 +96,14 @@ end
 
 function MplayerClass.setMute(self, mute) 
   if mute == true then
-		self:cmd("mute 1")
-	else
-		self:cmd("mute 0")
-	end
+    self:cmd("mute 1")
+  else
+    self:cmd("mute 0")
+  end
 end
 
 function MplayerClass.forward(self) 
-	self:cmd("seek +10")
+  self:cmd("seek +10")
 end
 
 function MplayerClass.forwardMore(self)
@@ -115,15 +115,15 @@ function MplayerClass.rewind(self)
 end
 
 function MplayerClass.rewindMore(self)
-	self:cmd("seek -300")
+  self:cmd("seek -300")
 end
 
 function MplayerClass.pause(self)
-	self:cmd("pause")
+  self:cmd("pause")
 end
 
 function MplayerClass.stop(self)
-	self:cmd("pause")
+  self:cmd("pause")
   self:cmd("stop")
   Janosh:set("/player/active","false")
 end
@@ -133,7 +133,7 @@ function MplayerClass.osd(self)
 end
 
 function MplayerClass.subtitle(self)
-	self:cmd("sub_visibility")
+  self:cmd("sub_visibility")
 end
 
 function MplayerClass.sotrack(self)
@@ -145,17 +145,17 @@ end
 
 function MplayerClass.eotrack(self) 
 print("eotrack")
-	obj = Janosh:get("/playlist/.")
+  obj = Janosh:get("/playlist/.")
   idx = tonumber(obj.index)
-	len = #obj.items
+  len = #obj.items
 print("len: " .. len)
 print("idx: " .. idx)
-	if idx < len then
-		self:jump(idx + 1)
-	else
-		Janosh:set("/player/active", "false")
-		self:stop()
-	end
+  if idx < len then
+    self:jump(idx + 1)
+  else
+    Janosh:set("/player/active", "false")
+    self:stop()
+  end
 end
 
 return MplayerClass:new()
--- src/lounge/lua/player.lua
+++ src/lounge/lua/player.lua
@@ -12,7 +12,7 @@ function open()
     ypid, ystdin, ystdout, ystderr = Janosh:popen("/usr/bin/youtube-dl","--encoding", "utf-8", "-g", "-e", url)
     title = Janosh:preadLine(ystdout)
     videoUrl = Janosh:preadLine(ystdout)
-		videoUrl = string.gsub(videoUrl, "https://", "http://")
+    videoUrl = string.gsub(videoUrl, "https://", "http://")
   else
     videoUrl = url
     title = basename(url)
