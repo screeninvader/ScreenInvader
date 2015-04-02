44b0489eabe4b5dfba726ed9bd44dd83dcc8e13f
kallaballa <amir@viel-zu.org>
2015-04-02 08:08:20 +0000

    non interactive mplayer build

--- third/build_mplayer.sh
+++ third/build_mplayer.sh
@@ -1,5 +1,5 @@
 cd /third/mplayer-export-*
 
-./configure --disable-dvdnav --disable-dvdread --enable-alsa --disable-mencoder --enable-x11 --enable-xv --enable-vdpau --enable-neon --enable-armvfp --enable-vfpv3 --extra-cflags=-mfpu=neon --extra-ldflags="-lm -lX11 -lXext"
+yes | ./configure --disable-dvdnav --disable-dvdread --enable-alsa --disable-mencoder --enable-x11 --enable-xv --enable-vdpau --enable-neon --enable-armvfp --enable-vfpv3 --extra-cflags=-mfpu=neon --extra-ldflags="-lm -lX11 -lXext"
 
 make
