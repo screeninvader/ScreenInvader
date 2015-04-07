609073a10df3c59d3d75c069d3e3ab59bb1d3db1
kallaballa <amir@viel-zu.org>
2015-04-07 17:42:36 +0200

    enable mplayer osd

--- third/build_mplayer.sh
+++ third/build_mplayer.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cd /third/mplayer-export-*
 
-yes | ./configure --disable-dvdnav --disable-dvdread --enable-alsa --disable-mencoder --enable-x11 --enable-xv --enable-vdpau --extra-ldflags="-lm -lX11 -lXext" --enable-neon --enable-armvfp --enable-vfpv3 --extra-cflags=-mfpu=neon
+yes | ./configure --enable-menu --disable-dvdnav --disable-dvdread --enable-alsa --disable-mencoder --enable-x11 --enable-xv --enable-vdpau --extra-ldflags="-lm -lX11 -lXext" --enable-neon --enable-armvfp --enable-vfpv3 --extra-cflags=-mfpu=neon
 
 make
 make install
