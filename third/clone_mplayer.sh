82eb416d16ba2d9ad2060dc2ae70f151e3d958e3
kallaballa <amir@viel-zu.org>
2015-04-08 12:56:19 +0000

    get ffmpeg snapshort too

--- third/clone_mplayer.sh
+++ third/clone_mplayer.sh
@@ -1,4 +1,8 @@
 rm -rf mplayer-export-*
 wget -c http://www.mplayerhq.hu/MPlayer/releases/mplayer-export-snapshot.tar.bz2
 tar -xjf mplayer-export-snapshot.tar.bz2
+cd mplayer-export-*
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar -xjf ffmpeg-snapshot.tar.bz2
+
 
