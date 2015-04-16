0c6f4e7996e2fc1de4fabe31d86fb26a454d43fc
kallaballa <amir@viel-zu.org>
2015-04-16 16:37:21 +0000

    remove obsolete file

--- third/build_janosh.sh
+++ third/build_janosh.sh
@@ -1,6 +1,6 @@
 cd /third/
 cd Janosh
-./build_dependencies.sh
+#./build_dependencies.sh
 export PATH="$PATH:/usr/local/bin/"
 make -j8 screeninvader_debug
 make DESTDIR=/ PREFIX=/lounge/ install
--- third/install_kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /third/linux-sunxi
-make ARCH=arm INSTALL_MOD_PATH=/ modules_install
-cp arch/arm/boot/uImage /boot/
-
