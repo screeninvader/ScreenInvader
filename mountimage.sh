b1bfcfb271a5b73891f41a49d92d75bfa79b70bc
kallaballa <amir@viel-zu.org>
2015-04-17 11:57:28 +0000

    more helper scripts

---
+++ mountimage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INFO="`kpartx -av "$1"`"
+PART1="`echo "$INFO" | sed 2d | cut -d" " -f3`"
+PART2="`echo "$INFO" | sed 1d | cut -d" " -f3`"
+
+mkdir -p "$2/p1"
+mkdir -p "$2/p2"
+sleep 3
+mount /dev/mapper/$PART1 "$2/p1"
+mount /dev/mapper/$PART2 "$2/p2"
+
---
+++ umountimage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+umount "$2/p1"
+umount "$2/p2"
+
+kpartx -d "$1"
+
