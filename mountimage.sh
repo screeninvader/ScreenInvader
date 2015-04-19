fad1771a36c438f5ede4e87991481fcd83dd53c3
kallaballa <amir@viel-zu.org>
2015-04-19 20:21:38 +0000

    try to umounting target directory before mounting in mountimage.sh

--- mountimage.sh
+++ mountimage.sh
@@ -6,6 +6,9 @@ PART2="`echo "$INFO" | sed 1d | cut -d" " -f3`"
 
 mkdir -p "$2/p1"
 mkdir -p "$2/p2"
+umount "$2/p1"
+umount "$2/p2"
+
 sleep 3
 mount /dev/mapper/$PART1 "$2/p1"
 mount /dev/mapper/$PART2 "$2/p2"
