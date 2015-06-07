5faaf7a48c834b150f5f1be10f8ef8acfcffe853
kallaballa <amir@viel-zu.org>
2015-06-07 14:00:35 +0000

    don't reretrieve go archive

--- third/clone_ipfs.sh
+++ third/clone_ipfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf go
-wget https://storage.googleapis.com/golang/go1.4.2.src.tar.gz
+wget -c https://storage.googleapis.com/golang/go1.4.2.src.tar.gz
 tar -xzf go1.4.2.src.tar.gz
 
