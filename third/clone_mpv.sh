f05e44a996a72493c431bfc914c640d8458aaf36
kallaballa <amir@viel-zu.org>
2016-10-28 14:13:28 +0000

    workaround for release images to prevent getting stuck with qemu-user-static during git-clone

--- third/clone_mpv.sh
+++ third/clone_mpv.sh
@@ -1,2 +1,9 @@
 rm -fr mpv-build
 git clone https://github.com/mpv-player/mpv-build.git
+cd mpv-build;
+git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
+git clone http://anongit.freedesktop.org/git/fribidi/fribidi.git fribidi
+git clone https://github.com/libass/libass.git libass
+git clone https://github.com/mpv-player/mpv.git mpv
+
+
